1a988ce0df6e0169b753105073decd0faa755517
davidkathoh <mudavidkat@gmail.com>
2017-03-22 18:02:24 -0700

    david  -dotsProgess

--- app/src/main/java/com/example/prinh/uhackathon/MainActivity.java
+++ app/src/main/java/com/example/prinh/uhackathon/MainActivity.java
@@ -7,8 +7,8 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.text.Html;
-import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -16,63 +16,47 @@ public class MainActivity extends FragmentActivity{
     private ViewPager viewPager;
     private TextView[] dots;
     private Button next;
+    private ImageView imageView;
     private LinearLayout linearLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         viewPager = (ViewPager)findViewById(R.id.pager);
-        next = (Button)findViewById(R.id.next);
-        linearLayout =(LinearLayout)findViewById(R.id.layoutdots);
        viewPager.setAdapter(new SimplePagerAdapter(getSupportFragmentManager()));
-       // addDots(0);
-        next.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                int current = getItem()+1;
-                if (current<2){
-                    viewPager.setCurrentItem(current);
-                }else{
-                    //launch home Activity
-                }
-            }
-        });
     }
 
-    public int getItem(){
-        return viewPager.getCurrentItem();
-    }
     private void addDots(int CurrentPosition) {
-        dots = new TextView[5];
+        dots = new TextView[2];
         int[] colorsActive = getResources().getIntArray(R.array.array_dot_active);
         int[] colorsInactive = getResources().getIntArray(R.array.array_dot_inactive);
 
         linearLayout.removeAllViews();
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < 2; i++) {
             dots[i] = new TextView(this);
             dots[i].setText(Html.fromHtml("&#45;"));
-            dots[i].setTextSize(40);
+            dots[i].setTextSize(35);
             dots[i].setTextColor(colorsInactive[CurrentPosition]);
             linearLayout.addView(dots[i]);
         }
         if (dots.length > 0)
             dots[CurrentPosition].setTextColor(colorsActive[CurrentPosition]);
     }
-        ViewPager.OnPageChangeListener viewPageListener = new ViewPager.OnPageChangeListener() {
-            @Override
-            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-            }
-            @Override
-            public void onPageSelected(int position) {
+//        ViewPager.OnPageChangeListener viewPageListener = new ViewPager.OnPageChangeListener() {
+//            @Override
+//            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+//            }
+//            @Override
+//            public void onPageSelected(int position) {
+//
+//            }
+//            @Override
+//            public void onPageScrollStateChanged(int state) {
+//
+//            }
+//        };
 
-            }
-            @Override
-            public void onPageScrollStateChanged(int state) {
-
-            }
-        };
-
-    public class SimplePagerAdapter extends FragmentPagerAdapter {
+    private class SimplePagerAdapter extends FragmentPagerAdapter {
 
 
         public SimplePagerAdapter(FragmentManager fm) {

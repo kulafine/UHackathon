356c5619257fd9901a40ef8d3389e6296048627d
davidkathoh <mudavidkat@gmail.com>
2017-03-23 10:34:31 -0700

    david  -dotsProgess

--- app/src/main/java/com/example/prinh/uhackathon/MainActivity.java
+++ app/src/main/java/com/example/prinh/uhackathon/MainActivity.java
@@ -7,8 +7,8 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.text.Html;
+import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -16,23 +16,39 @@ public class MainActivity extends FragmentActivity{
     private ViewPager viewPager;
     private TextView[] dots;
     private Button next;
-    private ImageView imageView;
     private LinearLayout linearLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         viewPager = (ViewPager)findViewById(R.id.pager);
+        next = (Button)findViewById(R.id.next);
+        linearLayout =(LinearLayout)findViewById(R.id.layoutdots);
        viewPager.setAdapter(new SimplePagerAdapter(getSupportFragmentManager()));
+        addDots(0);
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int current = getItem()+1;
+                if (current<4){
+                    viewPager.setCurrentItem(current);
+                }else{
+                    //launch home Activity
+                }
+            }
+        });
     }
 
+    public int getItem(){
+        return viewPager.getCurrentItem();
+    }
     private void addDots(int CurrentPosition) {
-        dots = new TextView[2];
+        dots = new TextView[5];
         int[] colorsActive = getResources().getIntArray(R.array.array_dot_active);
         int[] colorsInactive = getResources().getIntArray(R.array.array_dot_inactive);
 
         linearLayout.removeAllViews();
-        for (int i = 0; i < 2; i++) {
+        for (int i = 0; i < 4; i++) {
             dots[i] = new TextView(this);
             dots[i].setText(Html.fromHtml("&#45;"));
             dots[i].setTextSize(35);
@@ -42,21 +58,21 @@ public class MainActivity extends FragmentActivity{
         if (dots.length > 0)
             dots[CurrentPosition].setTextColor(colorsActive[CurrentPosition]);
     }
-//        ViewPager.OnPageChangeListener viewPageListener = new ViewPager.OnPageChangeListener() {
-//            @Override
-//            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-//            }
-//            @Override
-//            public void onPageSelected(int position) {
-//
-//            }
-//            @Override
-//            public void onPageScrollStateChanged(int state) {
-//
-//            }
-//        };
+        ViewPager.OnPageChangeListener viewPageListener = new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+            }
+            @Override
+            public void onPageSelected(int position) {
 
-    private class SimplePagerAdapter extends FragmentPagerAdapter {
+            }
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        };
+
+    public class SimplePagerAdapter extends FragmentPagerAdapter {
 
 
         public SimplePagerAdapter(FragmentManager fm) {
@@ -69,13 +85,13 @@ public class MainActivity extends FragmentActivity{
 
             switch (position) {
                 case 0:
-                    return new Learn();
-                case 1:
-                    return new Tools();
-                case 2:
-                    return new Prog_languages();
-                case 3:
                     return new Experience();
+                case 1:
+                    return new Prog_languages();
+                case 2:
+                    return new Experience();
+                case 3:
+                    return new Profile();
                 case 4:
                     return new Profile();
                 default:

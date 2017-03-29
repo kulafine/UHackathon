cb21e643754f94d7b510aae3eafa1b1f7b8f41a5
davidkathoh <mudavidkat@gmail.com>
2017-03-29 12:05:04 -0700

    questions

--- app/src/main/java/com/example/prinh/uhackathon/Question_three.java
+++ app/src/main/java/com/example/prinh/uhackathon/Question_three.java
@@ -1,23 +1,101 @@
 package com.example.prinh.uhackathon;
 
+import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 /**
  * Created by DAVID on 3/27/2017.
  */
 
 public class Question_three extends Fragment {
+    Spinner spinner;
+    ArrayList<String> languages = new ArrayList<String>();
     public Question_three() {
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstance) {
         View view = inflater.inflate(R.layout.question_three, container, false);
+      spinner = (Spinner) view.findViewById(R.id.spinnerCustom);
+        languages.add("YEAR ONE");
+        languages.add("YEAR TWO");
+        languages.add("YEAR THREE");
+  CustomSpinnerAdapter customSpinnerAdapter = new CustomSpinnerAdapter(getContext(), languages);
+        spinner.setAdapter(customSpinnerAdapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
 
-        return  view;
+                String item = parent.getItemAtPosition(position).toString();
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        return view;
+    }
+
+    public class CustomSpinnerAdapter extends BaseAdapter implements SpinnerAdapter {
+
+        private final Context activity;
+        private ArrayList<String> asr;
+
+        public CustomSpinnerAdapter(Context context, ArrayList<String> asr) {
+            this.asr = asr;
+            activity = context;
+        }
+
+
+        public int getCount() {
+            return asr.size();
+        }
+
+        public Object getItem(int i) {
+            return asr.get(i);
+        }
+
+        public long getItemId(int i) {
+            return (long) i;
+        }
+
+        @Override
+        public View getDropDownView(int position, View convertView, ViewGroup parent) {
+            TextView txt = new TextView(getContext());
+            txt.setPadding(16, 16, 16, 16);
+            txt.setTextSize(18);
+            txt.setGravity(Gravity.CENTER_VERTICAL);
+            txt.setText(asr.get(position));
+            txt.setTextColor(Color.parseColor("#14a895"));
+            return  txt;
+        }
+
+        public View getView(int i, View view, ViewGroup viewgroup) {
+            TextView txt = new TextView(getContext());
+            txt.setGravity(Gravity.CENTER);
+            txt.setPadding(16, 16, 16, 16);
+            txt.setTextSize(16);
+            txt.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.down, 0);
+            txt.setText(asr.get(i));
+            txt.setTextColor(Color.parseColor("#14a895"));
+            return  txt;
+        }
+
     }
 }
--- app/src/main/java/com/example/prinh/uhackathon/question_one.java
+++ app/src/main/java/com/example/prinh/uhackathon/question_one.java
@@ -22,8 +22,7 @@ import java.util.ArrayList;
  */
 
 public class question_one extends Fragment {
-    Spinner spinner;
-    ArrayList<String> languages = new ArrayList<String>();
+
 
     public question_one() {
     }
@@ -31,74 +30,10 @@ public class question_one extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstance) {
         View view = inflater.inflate(R.layout.question_one, container, false);
-//        spinner = (Spinner) view.findViewById(R.id.spinnerCustom);
-        languages.add("YEAR ONE");
-        languages.add("YEAR TWO");
-        languages.add("YEAR THREE");
-        CustomSpinnerAdapter customSpinnerAdapter = new CustomSpinnerAdapter(getContext(), languages);
-        spinner.setAdapter(customSpinnerAdapter);
-        spinner.setOnItemSelectedListener(new OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-
-                String item = parent.getItemAtPosition(position).toString();
-
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-
-            }
-        });
 
         return view;
     }
 
-    public class CustomSpinnerAdapter extends BaseAdapter implements SpinnerAdapter {
 
-        private final Context activity;
-        private ArrayList<String> asr;
-
-        public CustomSpinnerAdapter(Context context, ArrayList<String> asr) {
-            this.asr = asr;
-            activity = context;
-        }
-
-
-        public int getCount() {
-            return asr.size();
-        }
-
-        public Object getItem(int i) {
-            return asr.get(i);
-        }
-
-        public long getItemId(int i) {
-            return (long) i;
-        }
-
-        @Override
-        public View getDropDownView(int position, View convertView, ViewGroup parent) {
-            TextView txt = new TextView(getContext());
-            txt.setPadding(16, 16, 16, 16);
-            txt.setTextSize(18);
-            txt.setGravity(Gravity.CENTER_VERTICAL);
-            txt.setText(asr.get(position));
-            txt.setTextColor(Color.parseColor("#14a895"));
-            return  txt;
-        }
-
-        public View getView(int i, View view, ViewGroup viewgroup) {
-            TextView txt = new TextView(getContext());
-            txt.setGravity(Gravity.CENTER);
-            txt.setPadding(16, 16, 16, 16);
-            txt.setTextSize(16);
-            txt.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.down, 0);
-            txt.setText(asr.get(i));
-            txt.setTextColor(Color.parseColor("#14a895"));
-            return  txt;
-        }
-
-    }
 }
 
